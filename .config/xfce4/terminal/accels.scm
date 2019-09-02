0d908c848de7349861e827e4c85b06f089d10772
AN3223 <35916076+AN3223@users.noreply.github.com>
2019-09-01 21:57:09 -0500

    Fixed C-0 in vim

---
+++ .config/xfce4/terminal/accels.scm
@@ -0,0 +1,7 @@
+; xfce4-terminal GtkAccelMap rc-file         -*- scheme -*-
+; this file is an automated accelerator map dump
+; sudo chattr +i this file, or else it will be overwritten relentlessly
+;
+(gtk_accel_path "<Actions>/terminal-window/toggle-menubar" "F10")
+(gtk_accel_path "<Actions>/terminal-window/zoom-reset" "")
+
