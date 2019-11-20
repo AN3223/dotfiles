1a3068dfcfaaf70ba717001c4075965005251d3c
AN3223 <ethanr2048@gmail.com>
2019-11-20 02:17:29 -0600

    Wrote a plugin to avoid flooding my logs

---
+++ .config/mpv/scripts/shutup.c
@@ -0,0 +1,22 @@
+/*
+This plugin will set mpv's "quiet" option when stdout is NOT pointing
+to a terminal.
+
+gcc -o shutup.so shutup.c `pkg-config --cflags mpv` -shared -fPIC
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <mpv/client.h>
+
+int
+mpv_open_cplugin(mpv_handle *handle)
+{
+	if (!isatty(STDOUT_FILENO)) {
+		printf("shutup: setting --quiet\n");
+		mpv_set_property_string(handle, "quiet", "yes");
+	}
+	return 0;
+}
+
