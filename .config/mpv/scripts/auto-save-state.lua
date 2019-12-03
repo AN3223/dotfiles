b804e2cd75d7871e8f7fa580506f26f9311866c3
AN3223 <ethanr2048@gmail.com>
2019-12-03 15:38:22 -0600

    Picked up some inspiration from Hakkin's autosave script
    https://gist.github.com/Hakkin/5489e511bd6c8068a0fc09304c9c5a82

--- .config/mpv/scripts/auto-save-state.lua
+++ .config/mpv/scripts/auto-save-state.lua
@@ -1,17 +1,22 @@
 -- Runs write-watch-later-config every minute while the file is playing,
 -- and every time the file is paused.
 
-function write_watch_later()
+local options = require 'mp.options'
+local o = { save_interval = 60 }
+options.read_options(o)
+
+local function save()
+	mp.commandv("set", "msg-level", "cplayer=warn")
 	mp.command("write-watch-later-config")
+	-- FIXME this overwrites msg-level=cplayer=? original value
+	mp.commandv("set", "msg-level", "cplayer=status")
 end
 
-function save_if_pause(_, pause)
-	if pause then
-		write_watch_later()
-	end
+local function save_if_pause(_, pause)
+	if pause then save() end
 end
 
-function periodically_save(_, pause)
+local function periodically_save(_, pause)
 	if pause then
 		timer:stop()
 	end
@@ -21,8 +26,8 @@ function periodically_save(_, pause)
 	end
 end
 
-mp.observe_property("pause", "bool", save_if_pause)
-
-timer = mp.add_periodic_timer(60, write_watch_later)
+timer = mp.add_periodic_timer(o.save_interval, save)
 mp.observe_property("pause", "bool", periodically_save)
+mp.observe_property("pause", "bool", save_if_pause)
+mp.register_event("file-loaded", save)
 
