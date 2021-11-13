532f7534720d907b9815a861289ae6898ee6a48e
AN3223 <ethanr2048@gmail.com>
2021-11-12 22:50:32 -0600

    mpv: replace applicable scripts with conditional auto profiles

--- .config/mpv/scripts/force-window-profile.lua
+++ .config/mpv/scripts/force-window-profile.lua
@@ -1,22 +1,9 @@
--- This script uses a profile to change mpv's behavior when force-window is set
--- (i.e. by pseudo-gui in the .desktop file). This exists because:
+-- This script has been obsoleted by profile-cond.
 --
--- 1. Fullscreen doesn't make much sense when the window is going to
--- be entirely black or a still image (fullscreen=no)
+-- mpv.conf:
 --
--- 2. If you're going to have a window you might as well put some kind
--- of graphic in it when possible (audio-display=attachment)
---
--- There may be more options that don't make sense during --force-window
--- which haven't occurred to me, so I've implemented this as a profile so
--- it may be extended in the users config file.
---
--- Example usage of this script in mpv.conf:
 -- [force-window]
--- fullscreen=no
--- audio-display=attachment
-
-if mp.get_property("force-window") ~= "no" then
-	mp.command("apply-profile force-window")
-end
-
+-- profile-cond=force_window ~= false
+-- profile-restore=copy-equal
+-- ...
+--
--- .config/mpv/scripts/when-to-loop.lua
+++ .config/mpv/scripts/when-to-loop.lua
@@ -1,59 +1,15 @@
--- Sets loop-playlist=inf when a condition is true, conditions are kept in
--- $MPV_HOME/script-opts/when-to-loop.conf and the defaults go as follows:
+-- This script has been obsoleted by profile-cond.
 --
--- shuffle=no    # (loop if shuffled)
--- duration_lt=0 # (loop if the file is longer than 0 seconds)
--- playlist_gt=0 # (loop if the playlist contains more than 0 files)
+-- mpv.conf:
 --
--- duration_lt and playlist_gt are checked every file-loaded event and shuffle
--- is only checked on launch.
+-- # 10 and 100 are arbitrary, remove conditions that don't fit your needs
+-- [autoloop]
+-- profile-cond=shuffle or (duration < 10 and playlist_count == 1) or playlist_count > 100
+-- profile-restore=copy-equal
+-- loop-playlist=inf
 --
--- A value of 0 for duration_lt or playlist_gt means they won't be used to
--- determine when to loop
---
--- duration_lt is ignored if the playlist contains more than 1 entry
---
-
-require "mp.options"
-
-o = {
-	shuffle = false,
-	duration_lt = 0,
-	playlist_gt = 0
-}
-read_options(o, "when-to-loop")
-
-if o.shuffle and mp.get_property("shuffle") == "yes" then
-	mp.msg.info("shuffle=yes so loop-playlist=inf")
-	mp.set_property("loop-playlist", "inf")
-end
-
-local function loop()
-	-- use loop-file instead of loop-playlist if the playlist only contains 1
-	-- file. should be practically equivalent to loop-playlist, but the cache
-	-- won't be disposed of when the file ends, so this should prevent a delay
-	-- when looping a video over a network
-	if mp.get_property_number("playlist-count") == 1 then
-		mp.set_property("loop-file", "inf")
-	else
-		mp.set_property("loop-playlist", "inf")
-	end
-end
-
-local function file_loaded()
-	local duration = mp.get_property_number("duration")
-	local playlist_count = mp.get_property_number("playlist-count")
-	if o.duration_lt > 0 and playlist_count == 1 and duration < o.duration_lt then
-		mp.msg.info("duration of", duration, "less than",
-			 o.duration_lt, "so we're going to loop")
-		loop()
-	end
-
-	if o.playlist_gt > 0 and playlist_count > o.playlist_gt then
-		mp.msg.info("playlist-count of", playlist_count, "greater than",
-			 o.playlist_gt, "so we're going to loop")
-		loop()
-	end
-end
-
-mp.register_event("file-loaded", file_loaded)
+-- # optionally:
+-- [fastloop]
+-- profile-cond=loop_playlist == "inf" and playlist_count == 1
+-- profile-restore=copy-equal
+-- loop-file=inf
