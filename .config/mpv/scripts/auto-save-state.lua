26984ff19220d5893fb6668cedd4324ce9ae630f
AN3223 <ethanr2048@gmail.com>
2021-12-19 23:18:26 -0600

    auto-save-state: fix behavior when keep-open=yes

--- .config/mpv/scripts/auto-save-state.lua
+++ .config/mpv/scripts/auto-save-state.lua
@@ -1,13 +1,13 @@
 -- Runs write-watch-later-config periodically
 
-local utils = require 'mp.utils'
 local options = require 'mp.options'
-local msg = require 'mp.msg'
 local o = { save_interval = 60 }
 options.read_options(o)
 
 local function save()
-	mp.command("write-watch-later-config")
+	if mp.get_property_bool("save-position-on-quit") then
+		mp.command("write-watch-later-config")
+	end
 end
 
 local function save_if_pause(_, pause)
@@ -15,30 +15,42 @@ local function save_if_pause(_, pause)
 end
 
 local function pause_timer_while_paused(_, pause)
-	if pause then
-		timer:stop()
-	else
-		timer:resume()
-	end
+	if pause then timer:stop() else timer:resume() end
 end
 
--- This function is called on file-loaded instead of end-file because
--- the next file in the playlist would likely be loaded by the time the
--- end-file event runs.
+-- This function runs on file-loaded, registers two callback functions, and 
+-- then they run delete-watch-later-config when appropriate.
 local function delete_watch_later(event)
-	function end_file(path)
-		return function(event)
-			mp.unregister_event(end_file(path))
-			if event["reason"] == "eof" or event["reason"] == "stop" then
-				mp.commandv("delete-watch-later-config", path)
-			end
+	local path = mp.get_property("path")
+
+	-- Temporarily disables save-position-on-quit while eof-reached is true, so 
+	-- state isn't saved at EOF when keep-open=yes
+	local function eof_reached(_, eof)
+		if eof then
+			print("Deleting state (eof-reached)")
+			mp.commandv("delete-watch-later-config", path)
+			mp.set_property("save-position-on-quit", "no")
+		else
+			mp.set_property("save-position-on-quit", "yes")
 		end
 	end
 
-	mp.register_event("end-file", end_file(mp.get_property("path")))
+	local function end_file(event)
+		mp.unregister_event(end_file)
+		mp.unobserve_property(eof_reached)
+		if event["reason"] == "eof" or event["reason"] == "stop" then
+			print("Deleting state (end-file "..event["reason"]..")")
+			mp.commandv("delete-watch-later-config", path)
+		end
+	end
+
+	mp.observe_property("eof-reached", "bool", eof_reached)
+	mp.register_event("end-file", end_file)
 end
 
+mp.set_property("save-position-on-quit", "yes")
 timer = mp.add_periodic_timer(o.save_interval, save)
 mp.observe_property("pause", "bool", pause_timer_while_paused)
 mp.observe_property("pause", "bool", save_if_pause)
 mp.register_event("file-loaded", delete_watch_later)
+
