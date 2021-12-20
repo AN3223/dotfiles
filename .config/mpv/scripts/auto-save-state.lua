2927622a950d10932ec6a94e1f4207d8e75bb1d2
AN3223 <ethanr2048@gmail.com>
2021-12-19 21:19:07 -0600

    auto-save-state: more descriptive function names

--- .config/mpv/scripts/auto-save-state.lua
+++ .config/mpv/scripts/auto-save-state.lua
@@ -25,20 +25,20 @@ end
 -- This function is called on file-loaded instead of end-file because
 -- the next file in the playlist would likely be loaded by the time the
 -- end-file event runs.
-local function clean_watch_later(event)
-	function delete_watch_later_config(path)
+local function delete_watch_later(event)
+	function end_file(path)
 		return function(event)
-			mp.unregister_event(delete_watch_later_config(path))
+			mp.unregister_event(end_file(path))
 			if event["reason"] == "eof" or event["reason"] == "stop" then
 				mp.commandv("delete-watch-later-config", path)
 			end
 		end
 	end
 
-	mp.register_event("end-file", delete_watch_later_config(mp.get_property("path")))
+	mp.register_event("end-file", end_file(mp.get_property("path")))
 end
 
 timer = mp.add_periodic_timer(o.save_interval, save)
 mp.observe_property("pause", "bool", pause_timer_while_paused)
 mp.observe_property("pause", "bool", save_if_pause)
-mp.register_event("file-loaded", clean_watch_later)
+mp.register_event("file-loaded", delete_watch_later)
