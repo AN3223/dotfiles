3f08e0d5a533a4fdbb32f0b2b47446be50c6dfe3
AN3223 <ethanr2048@gmail.com>
2023-04-24 10:36:49 -0500

    gol.glsl: let decayed cells linger without dying

--- .config/mpv/shaders/fun/gol.glsl
+++ .config/mpv/shaders/fun/gol.glsl
@@ -1,19 +1,31 @@
 // vi: ft=c
 
-// Conway's Game of Life
+/* Conway's Game of Life
+ *
+ * Requires vo=gpu-next
+ *
+ * Works by computing a laplacian image and using the edges as GOL seeds
+ */
 
 //!HOOK LUMA
 //!BIND LUMA
 //!BIND GOL
 
-#define VISUALIZATION mix(poi.x, 1-poi.x, poi_gol)
-//#define VISUALIZATION (poi.x - poi_gol)
+// enable if you don't want cells to linger in non-edge areas
+#define DECAY_DEATH 0
 
+// if death is enabled, cells die after they decay below this value
+// otherwise, cells just stop decaying at this value
+#define DEATH_THRESHOLD 0.05
+
+// lower numbers decay faster
 #define DECAY 0.9
-#define DEATH 0.05
 
-// reset at this frame interval
-//#define RESET (24*30)
+// decays near frame edges
+#define EDGE_DECAY 0.05
+
+// lower numbers seed more
+#define LAPLACIAN_THRESHOLD 0.1
 
 vec4 hook()
 {
@@ -22,20 +34,27 @@ vec4 hook()
 	int live_neighbors = 0;
 	float avg_live_neighbor = 0;
 	float laplacian = poi.x * 8;
-	float poi_gol = clamp(imageLoad(GOL, ivec2(LUMA_pos*LUMA_size)).x, 0.0, 1.0);
+	float poi_cell = clamp(imageLoad(GOL, ivec2(LUMA_pos*LUMA_size)).x, 0.0, 1.0);
 
-	if (poi_gol < DEATH)
-		poi_gol = 0;
+	float edge_decay = min(1.0, min(
+		min(LUMA_pos.x*LUMA_size.x, LUMA_pos.y*LUMA_size.y)*EDGE_DECAY,
+		min(LUMA_size.x-LUMA_pos.x*LUMA_size.x, LUMA_size.y-LUMA_pos.y*LUMA_size.y)*EDGE_DECAY
+	));
+
+#if DECAY_DEATH
+	if (poi_cell < DEATH_THRESHOLD)
+		poi_cell = 0;
+#endif
 
 // donut increment, never lands on (0,0)
 #define DINCR(z,c) (z.c++,(z.c += int(z == vec2(0))))
 	for (neighbor.x = -1; neighbor.x <= 1; DINCR(neighbor,x))
 	for (neighbor.y = -1; neighbor.y <= 1; DINCR(neighbor,y)) {
-		float gol = imageLoad(GOL, ivec2((LUMA_pos + LUMA_pt * neighbor) * LUMA_size)).x;
+		float cell = imageLoad(GOL, ivec2((LUMA_pos + LUMA_pt * neighbor) * LUMA_size)).x + 0.001;
 
-		if (gol > DEATH) {
+		if (cell >= DEATH_THRESHOLD) {
 			live_neighbors++;
-			avg_live_neighbor += gol;
+			avg_live_neighbor += cell;
 		}
 
 		laplacian -= LUMA_texOff(neighbor).x;
@@ -43,20 +62,20 @@ vec4 hook()
 	laplacian = abs(laplacian / 8); // normalize to 0-1
 	avg_live_neighbor /= live_neighbors;
 
-	if (poi_gol > 0) {
+	if (poi_cell > 0) {
 		if (live_neighbors < 2 || live_neighbors > 3) // {under,over}population
-			poi_gol = 0;
+			poi_cell = 0;
 		else if (live_neighbors >= 2) // survive
-			poi_gol = poi_gol * DECAY;
+			poi_cell = max(poi_cell*DECAY*edge_decay, DEATH_THRESHOLD-DECAY_DEATH);
 	} else if (live_neighbors == 3) { // reproduction
-		poi_gol = avg_live_neighbor;
+		poi_cell = max(avg_live_neighbor, DEATH_THRESHOLD);
 	}
 
-	if (laplacian > DEATH) // seed
-		poi_gol = DECAY;
+	if (laplacian > LAPLACIAN_THRESHOLD) // seed
+		poi_cell = DECAY * edge_decay;
 
-	imageStore(GOL, ivec2(LUMA_pos*LUMA_size), vec4(poi_gol, 0, 0, 0));
-	return vec4(clamp(VISUALIZATION, 0.0, 1.0), 0, 0, poi.a);
+	imageStore(GOL, ivec2(LUMA_pos*LUMA_size), vec4(poi_cell, 0, 0, 0));
+	return vec4(clamp(mix(poi.x, 1-poi.x, poi_cell*edge_decay), 0.0, 1.0), 0, 0, poi.a);
 }
 
 //!TEXTURE GOL
