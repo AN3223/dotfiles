f0be0445106b660d2a581e10d127fd7653996a14
AN3223 <ethanr2048@gmail.com>
2023-04-24 05:02:15 -0500

    gol.glsl: implement a decay system

--- .config/mpv/shaders/fun/gol.glsl
+++ .config/mpv/shaders/fun/gol.glsl
@@ -6,62 +6,57 @@
 //!BIND LUMA
 //!BIND GOL
 
-// seed if this condition is true
-#define SEED_IF (laplacian < 0.4 || laplacian > 0.6)
-//#define SEED_IF (poi.x < 0.25)
-
-#define VISUALIZATION mix(poi.x, poi_gol, 0.5)
+#define VISUALIZATION mix(poi.x, 1-poi.x, poi_gol)
 //#define VISUALIZATION (poi.x - poi_gol)
 
-// reset at this frame interval
-#define RESET (24*20)
+#define DECAY 0.9
+#define DEATH 0.05
 
-// whether or not to compute the laplacian image
-#define LAPLACIAN 1
+// reset at this frame interval
+//#define RESET (24*30)
 
 vec4 hook()
 {
-	int poi_gol;
 	vec2 neighbor;
 	vec4 poi = LUMA_texOff(0);
 	int live_neighbors = 0;
-	float laplacian = 0;
+	float avg_live_neighbor = 0;
+	float laplacian = poi.x * 8;
+	float poi_gol = clamp(imageLoad(GOL, ivec2(LUMA_pos*LUMA_size)).x, 0.0, 1.0);
 
-	for (neighbor.x = -1; neighbor.x <= 1; neighbor.x++)
-	for (neighbor.y = -1; neighbor.y <= 1; neighbor.y++) {
-		int gol = int(imageLoad(GOL, ivec2((LUMA_pos + LUMA_pt * neighbor) * LUMA_size)).x);
+	if (poi_gol < DEATH)
+		poi_gol = 0;
 
-		if (neighbor == vec2(0,0))
-			poi_gol = gol;
-		else
-			live_neighbors += gol;
+// donut increment, never lands on (0,0)
+#define DINCR(z,c) (z.c++,(z.c += int(z == vec2(0))))
+	for (neighbor.x = -1; neighbor.x <= 1; DINCR(neighbor,x))
+	for (neighbor.y = -1; neighbor.y <= 1; DINCR(neighbor,y)) {
+		float gol = imageLoad(GOL, ivec2((LUMA_pos + LUMA_pt * neighbor) * LUMA_size)).x;
 
-#if LAPLACIAN
-		if (neighbor == vec2(0,0))
-			laplacian += poi.x * 8;
-		else
-			laplacian += LUMA_texOff(neighbor).x * -1;
-#endif
+		if (gol > DEATH) {
+			live_neighbors++;
+			avg_live_neighbor += gol;
+		}
+
+		laplacian -= LUMA_texOff(neighbor).x;
 	}
-	laplacian = (laplacian + 8) / 16; // normalize to 0-1
+	laplacian = abs(laplacian / 8); // normalize to 0-1
+	avg_live_neighbor /= live_neighbors;
 
-	if (poi_gol == 1) {
+	if (poi_gol > 0) {
 		if (live_neighbors < 2 || live_neighbors > 3) // {under,over}population
 			poi_gol = 0;
 		else if (live_neighbors >= 2) // survive
-			poi_gol = 1;
+			poi_gol = poi_gol * DECAY;
 	} else if (live_neighbors == 3) { // reproduction
-		poi_gol = 1;
+		poi_gol = avg_live_neighbor;
 	}
 
-	if (SEED_IF)
-		poi_gol = 1;
-
-	if (frame % RESET == 0) // reset
-		poi_gol = 0;
+	if (laplacian > DEATH) // seed
+		poi_gol = DECAY;
 
 	imageStore(GOL, ivec2(LUMA_pos*LUMA_size), vec4(poi_gol, 0, 0, 0));
-	return vec4(clamp(VISUALIZATION, 0.0, 1.0), 0, 0, 1.0);
+	return vec4(clamp(VISUALIZATION, 0.0, 1.0), 0, 0, poi.a);
 }
 
 //!TEXTURE GOL
