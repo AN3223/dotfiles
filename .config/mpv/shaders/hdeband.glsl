f57c23308f4c9eba46ca7a3c1c6b2d81ab92c684
AN3223 <ethanr2048@gmail.com>
2023-05-22 17:07:25 -0500

    hdeband.glsl: support blurring multiple bands together at once

--- .config/mpv/shaders/hdeband.glsl
+++ .config/mpv/shaders/hdeband.glsl
@@ -31,19 +31,22 @@
 //!DESC hdeband
 
 // Higher numbers blur more when the neighbor run is further away
-#define SS 0.1
+#define SS 0.5
 
 // Higher numbers blur more when intensity varies more between bands
 #define SI 0.005
 
 // Starting weight, lower values give less weight to the input image
-#define SW 0.1
+#define SW 1.0
 
 // Bigger numbers search further, but slower
 #define RADIUS 16
 
 // Bigger numbers search further, but less accurate
 #define SPARSITY 0.0
+
+// Number of bands to blur together at once, more is slower
+#define BANDS 1
 
 // Bigger numbers search in more directions, slower (max 8)
 // Only 4 and 8 are symmetrical, everything else blurs directionally
@@ -63,6 +66,11 @@
 #define NOT(x) (1 - (x))
 #define AND *
 #define TERNARY(cond, x, y) ((x)*(cond) + (y)*NOT(cond))
+#ifdef LUMA_raw
+#define EQ(x,y) val(val(x) == val(y))
+#else
+#define EQ(x,y) val(equal(val(x),val(y)))
+#endif
 
 // from NLM
 #if defined(LUMA_raw)
@@ -91,8 +99,7 @@
 vec4 poi_ = HOOKED_texOff(0);
 val poi = val_swizz(poi_);
 
-const float r_scale = 1.0/float(RADIUS*DIRECTIONS);
-const float ss_scale = 1.0/float(SS) * r_scale;
+const float ss_scale = 1.0/float(SS);
 const float si_scale = 1.0/float(SI);
 
 vec4 hook()
@@ -121,49 +128,58 @@ vec4 hook()
 		case 7: direction = vec2(-1, 1); break;
 		}
 
-		// XXX support blurring more than two runs together at once
-		val prev = poi;
-		val runs = val(1);
-		val run1_size = val(1); // includes POI
-		val run2_size = val(0);
-
-		for (int i = 1; i <= RADIUS; i++) {
-			float sparsity = floor(i * SPARSITY);
-			float new_sparsity = sparsity - floor((i - 1) * SPARSITY);
-			val px = val_swizz(HOOKED_texOff(direction * i + sparsity * direction));
-			val is_run = val(step(abs(prev - px), val(TOLERANCE)));
-
-			runs += NOT(is_run);
-			val in_bounds = step(runs, val(2));
-			prev = TERNARY(NOT(is_run) AND in_bounds, px, prev);
-
-			// consider skipped pixels as runs if their neighbors are both runs
-			is_run += is_run AND new_sparsity;
-
-#ifdef LUMA_raw
-			run1_size += is_run AND val(runs == 1);
-			run2_size += is_run AND val(runs == 2);
-#else
-			run1_size += is_run AND val(equal(runs, val(1)));
-			run2_size += is_run AND val(equal(runs, val(2)));
-#endif
+		val pixels[BANDS+1];
+		val run_sizes[BANDS+1];
+		for (int i = 1; i < BANDS+1; i++) {
+			pixels[i] = val(-1);
+			run_sizes[i] = val(0);
 		}
 
-		val weight = val(1);
-		weight *= gaussian(1/run1_size * ss_scale);
-		weight *= gaussian(abs(poi - prev) * si_scale);
+		pixels[0] = poi; // XXX never changes, could refactor to save memory/time
+		run_sizes[0] = val(1);
+		val run_idx = val(0);
+
+		val prev = poi;
+		for (int i = 1; i <= RADIUS; i++) {
+			float sparsity = floor(i * SPARSITY);
+			val px = val_swizz(HOOKED_texOff((i + sparsity) * direction));
+			val is_run = step(abs(prev - px), val(TOLERANCE));
+
+			run_idx += NOT(is_run);
+
+			// consider skipped pixels as runs if their neighbors are both runs
+			float new_sparsity = sparsity - floor((i - 1) * SPARSITY);
+			is_run += is_run AND new_sparsity;
+
+			for (int j = 0; j < BANDS+1; j++) {
+				pixels[j] = TERNARY(EQ(run_idx, j), px, pixels[j]);
+				run_sizes[j] += is_run AND EQ(run_idx, j);
+				prev = TERNARY(EQ(run_idx, j), pixels[j], prev);
+			}
+		}
+
+		val distance = val(0);
+		val is_run = val(1);
+		for (int i = 1; i < BANDS+1; i++) {
+			val weight = val(1);
+			weight *= gaussian(1/(distance += run_sizes[i-1]) * ss_scale);
+			weight *= gaussian(abs(pixels[i-1] - pixels[i]) * si_scale);
+			weight *= is_run *= step(val(1), run_sizes[i]); // zero out non-runs
+			weight *= NOT(EQ(pixels[i], poi));
+			weight *= NOT(EQ(pixels[i], -1));
 
 // XXX if (weight == extremum_weight) px should be picked randomly to prevent directional blur
 #if STRATEGY == 0
-		sum += prev * weight;
-		total_weight += weight;
+			sum += pixels[i] * weight;
+			total_weight += weight;
 #elif STRATEGY == 1
-		extremum_px = TERNARY(step(weight, extremum_weight), prev, extremum_px);
-		extremum_weight = min(weight, extremum_weight);
+			extremum_px = TERNARY(step(weight, extremum_weight), pixels[i], extremum_px);
+			extremum_weight = min(weight, extremum_weight);
 #elif STRATEGY == 2
-		extremum_px = TERNARY(step(weight, extremum_weight), extremum_px, prev);
-		extremum_weight = max(weight, extremum_weight);
+			extremum_px = TERNARY(step(weight, extremum_weight), extremum_px, pixels[i]);
+			extremum_weight = max(weight, extremum_weight);
 #endif
+		}
 	}
 
 #if STRATEGY == 0
