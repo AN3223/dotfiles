aa7b3a6555a60c0a2da18f6e353c211e53b359e0
AN3223 <ethanr2048@gmail.com>
2023-07-31 12:51:30 -0500

    shaders/Makefile: refactor
    Now optimums/ has naming that more closely matches the shader naming
    which enables latest.sh to automatically map most shader names to their
    optimum files (as opposed to having each mapping added to the Makefile
    by hand).

--- .config/mpv/shaders/dev/optimums/latest.sh
+++ .config/mpv/shaders/dev/optimums/latest.sh
@@ -1,5 +1,24 @@
 #!/bin/sh -e
-# Returns the latest cfgopts from a optimum file
+#
+# ./latest.sh file [plane]
+#
+# Returns the latest cfgopts from an optimum file. If "plane" is 
+# specified then this script will treat "file" as a path to a shader and 
+# attempt to map that path to the appropriate optimum path.
+#
 
-sed '/^$/d ; /^\#/d' "$1" | tail -n 1 | cut -f 1
+file="$1"
+if [ "$2" ]; then
+	file="$1"
+	file="${file#../}"
+	file="${file%.*}"
+	case "$file" in
+		*/*) ;;
+		*)
+			file="DEFAULT/$file" ;;
+	esac
+	file="optimums/$file/$2"
+fi
+
+sed '/^$/d ; /^\#/d' "$file" | tail -n 1 | cut -f 1
 
