728dc2a70c48e702025baff28f87c72fed346572
AN3223 <ethanr2048@gmail.com>
2023-11-03 15:59:49 -0500

    cropmode.lua: keep crop centered, add diagonal keybinds, & more
    1. Cropped window keeps its center while its size is being changed
    2. Diagonal (hjklyubn or numpad) keybinds added for cropping & panning
    3. Aspect ratio system has been reworked
    
    Ctrl+a & Ctrl+z only cycle through aspect ratios now, Ctrl+s will apply
    the change. This way the filter doesn't need to be reapplied
    unnecessarily. It will also always try to reduce the size of the cropped
    window (a much better user interface).

--- .config/mpv/scripts/cropmode.lua
+++ .config/mpv/scripts/cropmode.lua
@@ -2,28 +2,18 @@
 --
 -- Shift+C to enter/exit crop mode. Filter will be printed to console on exit.
 -- 
--- Arrow keys or hjkl to alter crop size, hold Ctrl to pan instead.
+-- Arrow keys, numpad, or hjklyubn to crop. Hold Ctrl to pan instead.
 --
 -- c to pan to center.
 --
 -- a/z to double/halve precision, hold Shift to increment/decrement instead.
 --
--- Ctrl+a to crop height to aspect ratio, press again to cycle, also hold Shift to reverse.
--- Ctrl+z to crop width to aspect ratio, press again to cycle, also hold Shift to reverse.
+-- Ctrl+a/Ctrl+z to cycle through aspect ratios, Ctrl+s to apply
 --
--- All keys may be rebound via the script-binding. The naming schemes go like this:
+-- All keys may be rebound via script-binding, the names of the keybinds can be 
+-- found near the bottom of this script. For example, if you wanted to rebind C 
+-- as F in input.conf:
 --
--- C:           cropmode
--- a/z          (dbl|hlf)step
--- A/Z          (add|sub)step
--- Ctrl+a/z     aspect(height|width)
--- Ctrl+A/Z     aspect(height|width)rev
--- Arrows       (add|sub)[wh]
--- Shift+Arrows (add|sub)[xy]
--- hjkl         (add|sub)[wh]vi
--- HJKL         (add|sub)[xy]vi
---
--- For example, if you wanted to rebind C as F in input.conf:
 -- 	F script-binding cropmode
 --
 -- If you use an hwdec, it will need to be of the copy variety, e.g.,
@@ -36,15 +26,16 @@ local options = require 'mp.options'
 local o = {
 	step = 64,
 	ars = {
+		{ 1,  1 },
 		{ 16, 9 },
 		{ 9, 16 },
-		{ 1,  1 },
-		{ 2,  3 },
-		{ 4,  3 }
+		{ 4,  3 },
+		{ 2,  3 }
 	}
 }
 options.read_options(o)
 step,ars = o.step,o.ars
+ari = 1
 
 -- keep x/y/w/h in the bounds of the image & initialize them if not already
 local function bound()
@@ -62,12 +53,12 @@ end
 local function filter() return "crop="..w..":"..h..":"..x..":"..y end
 local function update() bound(); mp.commandv("vf", "add", "@crop:"..filter()) end
 
-local function addw() w=w+step; update() end
-local function addh() h=h+step; update() end
+local function addw() w=w+step; x=x-step/2; update() end
+local function addh() h=h+step; y=y-step/2; update() end
 local function addx() x=x+step; update() end
 local function addy() y=y+step; update() end
-local function subw() w=w-step; update() end
-local function subh() h=h-step; update() end
+local function subw() w=w-step; x=x+step/2; update() end
+local function subh() h=h-step; y=y+step/2; update() end
 local function subx() x=x-step; update() end
 local function suby() y=y-step; update() end
 local function center() bound(); x=(iw-w)/2; y=(ih-h)/2; update() end
@@ -75,11 +66,16 @@ local function addstep() step=step+1; mp.commandv("show-text", "step: "..step) e
 local function substep() step=step-1; mp.commandv("show-text", "step: "..step) end
 local function dblstep() step=step*2; mp.commandv("show-text", "step: "..step) end
 local function hlfstep() step=step/2; mp.commandv("show-text", "step: "..step) end
-local function incrari() ari=((not ari or ari==#ars) and 1 or ari+1) end
-local function decrari() ari=((not ari or ari==1) and #ars or ari-1) end
 local function showaspect() mp.commandv("show-text", ars[ari][1]..":"..ars[ari][2]) end
-local function aspectheight() h=w/ars[ari][1]*ars[ari][2]; showaspect(); update() end
-local function aspectwidth()  w=h/ars[ari][2]*ars[ari][1]; showaspect(); update() end
+local function incrari() ari=(ari==#ars and 1 or ari+1); showaspect() end
+local function decrari() ari=(ari==1 and #ars or ari-1); showaspect() end
+local function applyaspect()
+	local w_ = h/ars[ari][2]*ars[ari][1]
+	local h_ = w/ars[ari][1]*ars[ari][2]
+	if w_ <= w then w = w_ else h = h_ end
+	showaspect()
+	update()
+end
 
 local function cropmode()
 	if not mode_on then
@@ -87,10 +83,31 @@ local function cropmode()
 		mp.commandv("show-text", "Crop mode enabled")
 		x,y,w,h = nil; update();
 
+		mp.add_forced_key_binding("c",           "center",  center)
+		mp.add_forced_key_binding("a",           "dblstep", dblstep)
+		mp.add_forced_key_binding("z",           "hlfstep", hlfstep)
+		mp.add_forced_key_binding("A",           "addstep", addstep, { repeatable = true })
+		mp.add_forced_key_binding("Z",           "substep", substep, { repeatable = true })
+		mp.add_forced_key_binding("Ctrl+a",      "incrari", incrari)
+		mp.add_forced_key_binding("Ctrl+z",      "decrari", decrari)
+		mp.add_forced_key_binding("Ctrl+s",      "applyaspect", function() applyaspect() end)
 		mp.add_forced_key_binding("h",           "subwvi",  subw)
 		mp.add_forced_key_binding("j",           "addhvi",  addh)
 		mp.add_forced_key_binding("k",           "subhvi",  subh)
 		mp.add_forced_key_binding("l",           "addwvi",  addw)
+		mp.add_forced_key_binding("y",           "subwsubhvi",  function() subw(); subh(); end)
+		mp.add_forced_key_binding("u",           "addwsubhvi",  function() addw(); subh(); end)
+		mp.add_forced_key_binding("b",           "subwaddhvi",  function() subw(); addh(); end)
+		mp.add_forced_key_binding("n",           "addwaddhvi",  function() addw(); addh(); end)
+		mp.add_forced_key_binding("1",           "subwaddhnp", function() subw(); addh(); end)
+		mp.add_forced_key_binding("2",           "addhnp",  addh)
+		mp.add_forced_key_binding("3",           "addwaddhnp", function() addw(); addh(); end)
+		mp.add_forced_key_binding("4",           "subwnp",  subw)
+		mp.add_forced_key_binding("5",           "applyaspectnp", applyaspect)
+		mp.add_forced_key_binding("6",           "addwnp",  addw)
+		mp.add_forced_key_binding("7",           "subwsubhnp", function() subw(); subh(); end)
+		mp.add_forced_key_binding("8",           "subhnp",  subh)
+		mp.add_forced_key_binding("9",           "addwsubhnp", function() addw(); subh(); end)
 		mp.add_forced_key_binding("LEFT",        "subw",    subw)
 		mp.add_forced_key_binding("DOWN",        "addh",    addh)
 		mp.add_forced_key_binding("UP",          "subh",    subh)
@@ -99,25 +116,29 @@ local function cropmode()
 		mp.add_forced_key_binding("Ctrl+j",      "addyvi",  addy)
 		mp.add_forced_key_binding("Ctrl+k",      "subyvi",  suby)
 		mp.add_forced_key_binding("Ctrl+l",      "addxvi",  addx)
+		mp.add_forced_key_binding("Ctrl+y",      "subxsubyvi", function() subx(); suby(); end)
+		mp.add_forced_key_binding("Ctrl+u",      "addxsubyvi", function() addx(); suby(); end)
+		mp.add_forced_key_binding("Ctrl+b",      "subxaddyvi", function() subx(); addy(); end)
+		mp.add_forced_key_binding("Ctrl+n",      "addxaddyvi", function() addx(); addy(); end)
+		mp.add_forced_key_binding("Ctrl+1",      "subxaddynp", function() subx(); addy(); end)
+		mp.add_forced_key_binding("Ctrl+2",      "addynp",  addy)
+		mp.add_forced_key_binding("Ctrl+3",      "addxaddynp", function() addx(); addy(); end)
+		mp.add_forced_key_binding("Ctrl+4",      "subxnp",  subx)
+		mp.add_forced_key_binding("Ctrl+5",      "centernp",  center)
+		mp.add_forced_key_binding("Ctrl+6",      "addxnp",  addx)
+		mp.add_forced_key_binding("Ctrl+7",      "subxsubynp", function() subx(); suby(); end)
+		mp.add_forced_key_binding("Ctrl+8",      "subynp",  suby)
+		mp.add_forced_key_binding("Ctrl+9",      "addxsubynp", function() addx(); suby(); end)
 		mp.add_forced_key_binding("Ctrl+LEFT",   "subx",    subx)
 		mp.add_forced_key_binding("Ctrl+DOWN",   "addy",    addy)
 		mp.add_forced_key_binding("Ctrl+UP",     "suby",    suby)
 		mp.add_forced_key_binding("Ctrl+RIGHT",  "addx",    addx)
-		mp.add_forced_key_binding("c",           "center",  center)
-		mp.add_forced_key_binding("a",           "dblstep", dblstep)
-		mp.add_forced_key_binding("z",           "hlfstep", hlfstep)
-		mp.add_forced_key_binding("A",           "addstep", addstep, { repeatable = true })
-		mp.add_forced_key_binding("Z",           "substep", substep, { repeatable = true })
-		mp.add_forced_key_binding("Ctrl+a",      "aspectheight", function() incrari(); aspectheight() end)
-		mp.add_forced_key_binding("Ctrl+z",      "aspectwidth",  function() incrari(); aspectwidth()  end)
-		mp.add_forced_key_binding("Ctrl+A",      "aspectheightrev", function() decrari(); aspectheight() end)
-		mp.add_forced_key_binding("Ctrl+Z",      "aspectwidthrev",  function() decrari(); aspectwidth()  end)
 	else
 		mode_on = false
 		print(filter()); mp.commandv("show-text", filter());
 		mp.commandv("vf", "remove", "@crop")
 
-		local binds = { "aspectwidthrev", "aspectheightrev", "aspectwidth", "aspectheight", "substep", "addstep", "hlfstep", "dblstep", "center", "addx", "suby", "addy", "subx", "addxvi", "subyvi", "addyvi", "subxvi", "addw", "subh", "addh", "subw", "addwvi", "subhvi", "addhvi", "subwvi" }
+		local binds = { "center", "dblstep", "hlfstep", "addstep", "substep", "incrari", "decrari", "applyaspect", "subwvi", "addhvi", "subhvi", "addwvi", "subwsubhvi", "addwsubhvi", "subwaddhvi", "addwaddhvi", "subwaddhnp", "addhnp", "addwaddhnp", "subwnp", "applyaspectnp", "addwnp", "subwsubhnp", "subhnp", "addwsubhnp", "subw", "addh", "subh", "addw", "subxvi", "addyvi", "subyvi", "addxvi", "subxsubyvi", "addxsubyvi", "subxaddyvi", "addxaddyvi", "subxaddynp", "addynp", "addxaddynp", "subxnp", "centernp", "addxnp", "subxsubynp", "subynp", "addxsubynp", "subx", "addy", "suby", "addx" }
 		for k,v in pairs(binds) do
 			mp.remove_key_binding(v)
 		end
