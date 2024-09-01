5ae80cd86ef48f15eff9e17f5fa4ec466c511f69
AN3223 <ethanr2048@gmail.com>
2024-09-01 11:20:38 -0500

    multisocket: add script messages for adding/removing the socket

--- .config/mpv/scripts/multisocket.lua
+++ .config/mpv/scripts/multisocket.lua
@@ -1,19 +1,33 @@
 -- Creates a socket for every mpv instance. The sockets are named "mpvctl"
--- followed by a number, i.e. mpvctl5. The highest number is the most recent
--- instance, the lowest is the oldest.
+-- followed by a number, i.e. mpvctl5.
 
-if mp.get_property("input-ipc-server") == '' then
-	i = 0
-	while true do
-		filename = "/tmp/mpvctl" .. i
-		local file, msg, err = io.open(filename)
-		if file == nil and err ~= 6 then -- 6 = socket (maybe not portable?)
-			break
-		else
-			i = i + 1
-		end
-	end
+filename = ''
 
-	mp.set_property("input-ipc-server", filename)
-	mp.register_event("shutdown", function() os.remove(filename) end)
+function remove_socket()
+	print("Socket " .. filename .. " removed")
+	mp.set_property("input-ipc-server", "")
+	os.remove(filename)
 end
+
+function add_socket()
+	if mp.get_property("input-ipc-server") == "" then
+		i = 0
+		while true do
+			filename = "/tmp/mpvctl" .. i
+			local file, msg, err = io.open(filename)
+			if file == nil and err ~= 6 then -- 6 = socket (maybe not portable?)
+				break
+			else
+				i = i + 1
+			end
+		end
+
+		mp.set_property("input-ipc-server", filename)
+		mp.register_event("shutdown", remove_socket)
+	end
+end
+
+add_socket()
+mp.register_script_message("add-socket", add_socket)
+mp.register_script_message("remove-socket", remove_socket)
+
